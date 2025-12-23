cc419e80ba547bda86059eafc3c19194dc4cf749
XynthiaKlomp <xynthiaklomp@gmail.com>
2025-12-23 12:38:00 +0100

    Player movement
    player is able to move between points, you need to set the point yourself in walkingpoints. you can have 3 points to move between.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -20,7 +20,8 @@ var current_walk_point : VisibleOnScreenNotifier3D
 var last_walk_point : VisibleOnScreenNotifier3D
 var looking_at_walk_point : VisibleOnScreenNotifier3D
 
-enum look_dir {LEFT, MIDDLE, RIGHT}
+enum look_dir_3 {LEFT, MIDDLE, RIGHT}
+enum look_dir_2 {LEFT, RIGHT}
 
 func _init() -> void:
 	id = 1
@@ -38,21 +39,15 @@ func _physics_process(delta: float) -> void:
 	
 	if looking_at_walk_point && looking_at_walk_point != current_walk_point && Input.is_action_just_pressed("MoveFoward"):
 		set_new_position(looking_at_walk_point)
+		turn_to_walk_point_once_moved()
 	
 	
-	if looking_at_walk_point == null:
-		turn_to_walk_point(look_dir.MIDDLE)
-	
-	if looking_at_walk_point == last_walk_point:
-		turn_to_walk_point(look_dir.RIGHT)
-	#else:
-		#turn_to_walk_point(look_dir.LEFT)
 	
 	if Input.is_action_just_pressed("TurnLeft"):
-		turn_to_walk_point(look_dir.LEFT)
+		turn_to_walk_point(look_dir_2.LEFT)
 	
 	if Input.is_action_just_pressed("TurnRight"):
-		turn_to_walk_point(look_dir.RIGHT)
+		turn_to_walk_point(look_dir_2.RIGHT)
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -86,43 +81,71 @@ func _input(event: InputEvent) -> void:
 			screen_relative *= degrees_per_unit
 
 func set_new_position(new_position: VisibleOnScreenNotifier3D) -> void:
+	#if skipping a position check walkinpoints array
 	last_walk_point = current_walk_point
 	current_walk_point = new_position
 	position = new_position.position
 
 
 func get_looking_at_walk_point() -> VisibleOnScreenNotifier3D:
-	var points_next_to_current_point : Array = GameManager.walking_points.check_points_next_to_current_point(current_walk_point)
+	var walk_points_next_to_current_walk_point : Array = GameManager.walking_points.check_points_next_to_current_point(current_walk_point)
 	
-	for walk_point : VisibleOnScreenNotifier3D in points_next_to_current_point:
+	for walk_point : VisibleOnScreenNotifier3D in walk_points_next_to_current_walk_point:
 		if walk_point.is_on_screen():
 			return walk_point
 	
 	return null
 
 func turn_to_walk_point(direction: int) -> void:
+	var walk_points_next_to_current_walk_point : Array = GameManager.walking_points.check_points_next_to_current_point(current_walk_point)
+	
+	
+	if walk_points_next_to_current_walk_point.size() >= 3:
+		var current_look_dir : look_dir_3
+		
+		if direction == look_dir_2.RIGHT:
+			direction = look_dir_3.RIGHT
+		elif direction == look_dir_2.LEFT:
+			direction = look_dir_3.LEFT
+		
+		for walk_point : VisibleOnScreenNotifier3D in walk_points_next_to_current_walk_point:
+			if looking_at_walk_point == walk_point:
+				current_look_dir = walk_points_next_to_current_walk_point.find(walk_point, 0)
+		
+		match current_look_dir:
+			look_dir_3.LEFT:
+				if direction == look_dir_3.RIGHT:
+					look_at(walk_points_next_to_current_walk_point[look_dir_3.MIDDLE].position, up_direction)
+			look_dir_3.MIDDLE:
+				if direction == look_dir_3.LEFT:
+					look_at(walk_points_next_to_current_walk_point[look_dir_3.LEFT].position, up_direction)
+				else:
+					look_at(walk_points_next_to_current_walk_point[look_dir_3.RIGHT].position, up_direction)
+			look_dir_3.RIGHT:
+				if direction == look_dir_3.LEFT:
+					look_at(walk_points_next_to_current_walk_point[look_dir_3.MIDDLE].position, up_direction)
+	else:
+		var current_look_dir : look_dir_2
+		
+		for walk_point : VisibleOnScreenNotifier3D in walk_points_next_to_current_walk_point:
+			if looking_at_walk_point == walk_point:
+				var position = walk_points_next_to_current_walk_point.find(walk_point, 0)
+				if position == 1:
+					current_look_dir = look_dir_2.RIGHT
+				else:
+					current_look_dir = look_dir_2.LEFT
+	
+		match current_look_dir:
+			look_dir_2.LEFT:
+				if direction == look_dir_2.RIGHT:
+						look_at(walk_points_next_to_current_walk_point[look_dir_2.RIGHT].position, up_direction)
+			look_dir_2.RIGHT:
+				if direction == look_dir_2.LEFT:
+						look_at(walk_points_next_to_current_walk_point[look_dir_2.LEFT].position, up_direction)
+
+func turn_to_walk_point_once_moved() -> void:
 	var points_next_to_current_point : Array = GameManager.walking_points.check_points_next_to_current_point(current_walk_point)
-	var first_point_next_to_current_point : VisibleOnScreenNotifier3D
 	
-	# doesnt work because the left or rigth side depends on how the player walks but should be on how the player is facing.
-	
-	
-	
-	
-	if direction == look_dir.MIDDLE:
-		if points_next_to_current_point.size() <= 2:
-			pass
-		else:
-			first_point_next_to_current_point = points_next_to_current_point[1]
-	elif direction == look_dir.LEFT:
-		if points_next_to_current_point.size() <= 2:
-			first_point_next_to_current_point = points_next_to_current_point[0]
-		else:
-			first_point_next_to_current_point = points_next_to_current_point[0]
-	elif direction == look_dir.RIGHT:
-		if points_next_to_current_point.size() <= 2:
-			first_point_next_to_current_point = points_next_to_current_point[1]
-		else:
-			first_point_next_to_current_point = points_next_to_current_point[2]
-	
-	look_at(first_point_next_to_current_point.position, up_direction)
+	for walk_point : VisibleOnScreenNotifier3D in points_next_to_current_point:
+		if walk_point != last_walk_point:
+			look_at(walk_point.position, up_direction)
--- Scripts/walking_points.gd
+++ Scripts/walking_points.gd
@@ -1,41 +1,45 @@
 class_name WalkingPoints
 extends Node3D
 
+
 @onready var walking_point_1: VisibleOnScreenNotifier3D = $WalkingPoint1
 @onready var walking_point_2: VisibleOnScreenNotifier3D = $WalkingPoint2
 @onready var walking_point_3: VisibleOnScreenNotifier3D = $WalkingPoint3
 @onready var walking_point_4: VisibleOnScreenNotifier3D = $WalkingPoint4
-
-
+@onready var walking_point_5: VisibleOnScreenNotifier3D = $WalkingPoint5
+@onready var walking_point_6: VisibleOnScreenNotifier3D = $WalkingPoint6
 
 @onready var starting_point: VisibleOnScreenNotifier3D = walking_point_1
-
-
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameManager.walking_points = self
 	GameManager.player.current_walk_point = starting_point
-	
-
 
 func check_points_next_to_current_point(current_point : VisibleOnScreenNotifier3D) -> Array:
 	var points_next_to_current_point : Array[VisibleOnScreenNotifier3D]
 	
-	# points at the top will be the next looking at point for player. if you want a specific point to show first over another then that has to be on the top.
+	# points next to current point, in order of left, middle, right if 3. otherwise , left and right
 	match current_point:
 		walking_point_1:
+			points_next_to_current_point.push_back(walking_point_5)
 			points_next_to_current_point.push_back(walking_point_2)
 			points_next_to_current_point.push_back(walking_point_4)
 		walking_point_2:
 			points_next_to_current_point.push_back(walking_point_3)
 			points_next_to_current_point.push_back(walking_point_1)
+			points_next_to_current_point.push_back(walking_point_6)
 		walking_point_3:
 			points_next_to_current_point.push_back(walking_point_4)
 			points_next_to_current_point.push_back(walking_point_2)
 		walking_point_4:
 			points_next_to_current_point.push_back(walking_point_1)
 			points_next_to_current_point.push_back(walking_point_3)
+		walking_point_5:
+			points_next_to_current_point.push_back(walking_point_6)
+			points_next_to_current_point.push_back(walking_point_1)
+		walking_point_6:
+			points_next_to_current_point.push_back(walking_point_2)
+			points_next_to_current_point.push_back(walking_point_5)
 	
 	return points_next_to_current_point
